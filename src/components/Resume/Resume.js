dfd347b4b6f3e7c35d70faf6ed626f5b30e1cc13
Joshua Miller <joshsmiller1022@gmail.com>
2021-06-14 10:46:53 -0600

    resume comp done

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import resume from '../Resume/Resume.png'
+import resumePDF from '../Resume/Resume2021.pdf'
 
 const Resume = () => {
-    return <div>
-        Resume Component
+    return <div className="resume">
+        <a href={resumePDF} target="_blank" rel="noreferrer noopener">
+            <img src={resume} alt="Josh's Resume"></img>
+        </a>
     </div>
 }
 
