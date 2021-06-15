4e1e6124a9d730161754df4c2cf1cff63c2f0210
Joshua Miller <joshsmiller1022@gmail.com>
2021-06-15 11:15:39 -0600

    edits added to home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
+import { FaLinkedinIn} from 'react-icons/fa';
+import { AiFillGithub} from 'react-icons/ai';
 // import {Link} from 'react-router-dom';
 
-const Footer = () => {
-    return <div className="footer">
-        Footer Component
-    </div>
-}
+class Footer extends React.Component {
+    render() {
+      return <div className="footer">
+          <a href="https://www.linkedin.com/in/joshua-s-miller/" target="_blank" rel="noreferrer noopener">
+            <FaLinkedinIn />
+          </a>
+          <a href="https://github.com/jmillion3" target="_blank" rel="noreferrer noopener">
+            <AiFillGithub />
+          </a>
+      </div>
+    }
+  }
 
 export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import designer from '../Home/designer.jpg';
+import frontend from '../Home/frontend.jpg';
+import backend from '../Home/backended.jpg';
 
 const Home = () => {
     return <div className="home">
-        {/* <img src={background}></img> */}
         <h1>Hello, World!</h1>
-        <p>Welcome to my portfolio. My name is Josh Miller. I am a Front End Developer based out of Salt Lake City, UT.</p>
+        {/* <p>Welcome to my portfolio. My name is Josh Miller. I am a Front End Developer based out of Salt Lake City, UT.</p> */}
+        <p>Meet the team</p>
+        <div className="team">
+            <div className="designer">
+                <img src={designer} alt="Lead Designer"></img>
+                <h2>J. S. Miller</h2>
+                <p>Lead Designer</p>
+            </div>
+            <div className="frontend">
+                <img src={frontend} alt="Frontend Engineer"></img>
+                <h2>Joshua Miller</h2>
+                <p>Front End Developer</p>
+            </div>
+            <div className="backend">
+                <img src={backend} alt="Backend Engineer"></img>
+                <h2>Josh S. Miller</h2>
+                <p>Back End Developer</p>
+            </div>
+        </div>
     </div>
 }
 
